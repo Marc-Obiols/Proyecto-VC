c9614ccfc56d1d865e31497c882b52c0e506f0ef
Marc-Obiols <55504484+Marc-Obiols@users.noreply.github.com>
2019-12-15 18:20:34 +0100

    Update project.m

--- project.m
+++ project.m
@@ -10,6 +10,8 @@ y1 = rect(2);
 y2 = y1 + rect(4);
 % M = uint8(colfilt(I, [10, 10], 'sliding', @propiedades, rect)); 
 
+% HOG = extractHOGFeatures(I);
+
 [f,c,col] = size(I);
 BW = zeros(f,c);
 imshow(BW);
@@ -21,6 +23,8 @@ y = rect(2) + rect(3);
 BW(y1:y2, x1:x2) = 1;
 imshow(BW);
 Features = features(I, BW, 17);
+Test = MatrizTest(Features);
+
 
 %B = im2col(I,[17 17], 'distinct');
 
