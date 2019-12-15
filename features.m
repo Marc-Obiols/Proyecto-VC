f07bd2262c9f0ad1643261af47b256da0ca233fb
Marc-Obiols <55504484+Marc-Obiols@users.noreply.github.com>
2019-12-15 18:19:55 +0100

    Update features.m

--- features.m
+++ features.m
@@ -2,7 +2,7 @@ function [Features] = features(I,BW, tam)
 [f,c,col] = size(I);
 
 sizeF = ceil(f/tam)*ceil(c/tam);
-Features = zeros(sizeF,7);
+Features = zeros(sizeF,9);
 ContDef = 1;
 
 for i = 1:tam:f
@@ -13,36 +13,43 @@ for i = 1:tam:f
         RC = 0;
         GC = 0;
         BC = 0;
+        IL = 0;
+        ILC = 0;
         countU = 0;
         countL = 0;
         for k = i:1:i+tam-1
             for z = j:1:j+tam-1
-                if (k<f) && (z<c) 
+                if (k<f) && (z<c)
                    if BW(k,z) == 0
                        R = R + uint16(I(k,z,1));
                        G = G + uint16(I(k,z,2));
                        B = B + uint16(I(k,z,3));
+                       IL = IL + (uint16(I(k,z,1))+uint16(I(k,z,2))+uint16(I(k,z,3)))/3;
                    else
                        RC = RC + uint16(I(k,z,1));
                        GC = GC + uint16(I(k,z,2));
                        BC = BC + uint16(I(k,z,3));
-                       countU = countU + 1; %numero de 1
+                       ILC = ILC + (uint16(I(k,z,1))+uint16(I(k,z,2))+uint16(I(k,z,3)))/3;
+                       countU = countU + 1; % numero de 1
                    end
                 else
-                   countL = countL + 1; %numero de pixeles fuera de la ventana
+                   countL = countL + 1; % numero de pixeles fuera de la ventana
                 end
             end
         end
+        
         
         % media de color FONDO por ventana
         Features(ContDef,1) = R/(tam^2-countL-countU);
         Features(ContDef,2) = G/(tam^2-countL-countU);
         Features(ContDef,3) = B/(tam^2-countL-countU);
+        Features(ContDef,8) = IL/(tam^2-countL-countU);
         
         % media de color OBJETO por ventana
         Features(ContDef,4) = RC/(countU);
         Features(ContDef,5) = GC/(countU);
         Features(ContDef,6) = BC/(countU);
+        Features(ContDef,9) = ILC/(countU);
         
         if countU > 130 && countU < 160
             meanR = (R + RC)/(tam^2-countL);
