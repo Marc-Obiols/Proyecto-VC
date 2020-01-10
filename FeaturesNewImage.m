b6770271aef1ee130eafdcf92517d4188e51ceb0
Marc-Obiols <55504484+Marc-Obiols@users.noreply.github.com>
2020-01-10 01:59:29 +0100

    Update FeaturesNewImage.m

--- FeaturesNewImage.m
+++ FeaturesNewImage.m
@@ -5,7 +5,7 @@ function [FeatureWindow] = FeaturesNewImage(I, tam)
 
 sizeF = ceil(f/tam)*ceil(c/tam);
 Features = zeros(sizeF,9);
-FeatureWindow = zeros(sizeF,4);
+FeatureWindow = zeros(sizeF,13);
 ContDef = 1;
 
 for i = 1:tam:f
@@ -20,6 +20,8 @@ for i = 1:tam:f
         ILC = 0;
         countU = 0;
         countL = 0;
+        countFF = 0;
+        countCF = 0;
         for k = i:1:i+tam-1
             for z = j:1:j+tam-1
                 if (k<f) && (z<c)
@@ -29,15 +31,29 @@ for i = 1:tam:f
                        IL = IL + (uint16(I(k,z,1))+uint16(I(k,z,2))+uint16(I(k,z,3)))/3;           
                 else
                    countL = countL + 1; % numero de pixeles fuera de la ventana
+                   if (z > c)
+                       countCF = countCF + 1;
+                   end
                 end
             end
+            if (k > f)
+                countFF = countFF + 1;
+            end
         end
+        
+        countCF = countCF / 17;
         
         FeatureWindow(ContDef,1) =  R/(tam^2-countL-countU);
         FeatureWindow(ContDef,2) =  G/(tam^2-countL-countU);
         FeatureWindow(ContDef,3) =  B/(tam^2-countL-countU);
         FeatureWindow(ContDef,4) =  IL/(tam^2-countL-countU);
-
+        
+        xixa = I(i:1:(i+16-countFF), j:1:(j+16-countCF), 1:1:3);
+        [hog_2x2, validPoints] = extractHOGFeatures(xixa,'CellSize',[17-countFF 17-countCF], 'BlockSize', [1 1]);
+        [pxv pyv] = size(hog_2x2);
+        if pyv ~= 0
+            FeatureWindow(ContDef,5:1:13) = hog_2x2(1:1:9);
+        end
         ContDef = ContDef + 1;
     end
 end
