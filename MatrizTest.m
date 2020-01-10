2954d9a263e83943595696182b8b547da4ffabb5
Marc-Obiols <55504484+Marc-Obiols@users.noreply.github.com>
2020-01-10 01:58:55 +0100

    Update MatrizTest.m

--- MatrizTest.m
+++ MatrizTest.m
@@ -1,6 +1,6 @@
 function [res] = MatrizTest(v)
 [f c] = size(v);
-res = zeros(2,4);
+res = zeros(2,13);
 CountO = 0;
 CountU = 0;
 for i = 1:f
@@ -10,17 +10,21 @@ for i = 1:f
         res(1, 2) = res(1, 2) + v(i, 2);
         res(1, 3) = res(1, 3) + v(i, 3);
         res(1, 4) = res(1, 4) + v(i, 8);
+        res(1, 5:1:13) = res(1, 5:1:13) + v(i, 10:1:18);
     else
         CountU = CountU + 1;
         res(2, 1) = res(2, 1) + v(i, 4);
         res(2, 2) = res(2, 2) + v(i, 5);
         res(2, 3) = res(2, 3) + v(i, 6);
         res(2, 4) = res(2, 4) + v(i, 9);
+        res(2, 5:1:13) = res(2, 5:1:13) + v(i, 10:1:18);
     end
 end
 res(1, 1:1:3) = res(1, 1:1:3)/CountO;
 res(2, 1:1:3) = res(2, 1:1:3)/CountU;
 res(1, 4) = res(1, 4)/CountO;
 res(2, 4) = res(2, 4)/CountU;
+res(1, 5:1:13) = res(1, 5:1:13)/CountO;
+res(2, 5:1:13) = res(2, 5:1:13)/CountU;
 end
 
