275f1ed38f13763c1833f9ea0e158b6a1bea7233
Miguel Salamero <47792466+MiguelSalamero@users.noreply.github.com>
2020-01-14 19:37:50 +0100

    Add files via upload

--- project.m
+++ project.m
@@ -1,6 +1,6 @@
 [file,p1] = uigetfile(('*.bmp;*.jpg;*.png;'),"Image to segment");
 s1 = strcat(p1,file);
-[I, map] = imread(s1);
+[I, ~] = imread(s1);
     
 imshow(I);
 rect = getrect;
@@ -8,36 +8,79 @@ x1 = rect(1);
 x2 = x1 + rect(3);
 y1 = rect(2);
 y2 = y1 + rect(4);
-% M = uint8(colfilt(I, [10, 10], 'sliding', @propiedades, rect)); 
 
-% HOG = extractHOGFeatures(I);
 
 [f,c,col] = size(I);
 BW = zeros(f,c);
-% imshow(BW);
-% figure
 
 x = rect(1) + rect(4);
 y = rect(2) + rect(3);
 
 BW(y1:y2, x1:x2) = 1;
-% imshow(BW);
 
 Features = features(I, BW, 17);
 Test = MatrizTest(Features);
+
+
+% Prediction
+Modelo = trainClassifier(Test);
+
+
+% Object first image
+FeaturesWindow = FeaturesNewImage(I,17);
+
+% Matriz resultado: predicción de cada ventana
+[fFtWindow,~] = size(FeaturesWindow);
+ResultOG = zeros(fFtWindow,1);
+
+% Calcular predicción por cada ventana
+for i = 1:1:fFtWindow
+    ResultOG(i,1) = predict(Modelo.ClassificationKNN,FeaturesWindow(i,:));
+end
+
+% Pintar resultado
+ImResOG = PrintResult(ResultOG,I,17);
+
+% Tratar Imagen Resultado
+
+SE = strel('disk',11);
+ImResOG = imclose(ImResOG, SE);
+ImRes = imfill(ImResOG,'holes');
+ImResOG = imopen(ImResOG, SE);
+
+CC = bwconncomp(ImResOG);
+stat = regionprops(CC,'Centroid','Area','PixelIdxList');
+[~,index] = max([stat.Area]);
+ImResOG(:,:)=0;
+ImResOG(stat(index).PixelIdxList)=1;
+
+
+BlackWhite = edge(ImResOG);
+[fN,cN,~] = size(ImResOG);
+for i = 1:1:fN
+    for j = 1:1:cN
+        if BlackWhite(i, j) == 1
+            I(i,j,1) = 255;
+            I(i,j,2) = 0;
+            I(i,j,3) = 0;
+        end
+    end
+end
+
+figure('NumberTitle', 'off', 'Name','Object to segment')
+imshow(I);
+
+
 
 % Read New Image
 [file,p1] = uigetfile(('*.bmp;*.jpg;*.png;'),"Image to predict");
 s1 = strcat(p1,file);
 [NewImage, map] = imread(s1);
-% NewImage = imread('Eagle 2.png');
 
 % Extraer features de las ventanas de la nueva imagen
 FeaturesWindow = FeaturesNewImage(NewImage,17);
 
 % Prediction
-% Y = ['Background'; 'Foreground';];
-% T = table(Test,Y);
 Modelo = trainClassifier(Test);
 
 
@@ -54,7 +97,6 @@ end
 ImRes = PrintResult(Result,NewImage,17);
 
 % Tratar Imagen Resultado
-
 SE = strel('disk',11);
 ImRes = imclose(ImRes, SE);
 ImRes = imfill(ImRes,'holes');
@@ -66,8 +108,7 @@ stat = regionprops(CC,'Centroid','Area','PixelIdxList');
 ImRes(:,:)=0;
 ImRes(stat(index).PixelIdxList)=1;
 
-figure
-imshow(edge(ImRes))
+
 BlackWhite = edge(ImRes);
 [fN,cN,colN] = size(NewImage);
 for i = 1:1:fN
@@ -80,5 +121,5 @@ for i = 1:1:fN
     end
 end
 
-figure
+figure('NumberTitle', 'off', 'Name','Predicted Object')
 imshow(NewImage);
